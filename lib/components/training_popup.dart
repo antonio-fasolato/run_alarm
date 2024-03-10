7f1c75c34cdf88933a9ce1437ef2b4bb04e09ee6
Antonio Fasolato <antonio.fasolato@gmail.com>
2024-03-10 11:06:50 +0100

    Form layout

--- lib/components/training_popup.dart
+++ lib/components/training_popup.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class TrainingPopup extends StatelessWidget {
+class TrainingPopup extends StatefulWidget {
   const TrainingPopup({super.key});
 
   @override
+  State<TrainingPopup> createState() => _TrainingPopupState();
+}
+
+class _TrainingPopupState extends State<TrainingPopup> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    var i18n = AppLocalizations.of(context);
+    if (i18n == null) {
+      throw Exception("No localization");
+    }
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              label: Text(i18n.trainingPopupTitle),
+            ),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) =>
+                (value ?? '').isEmpty ? i18n.trainigPopupTitleValidation : null,
+          ),
+          TextFormField(
+            controller: _descriptionController,
+            decoration: InputDecoration(
+              label: Text(i18n.trainingPopupDescription),
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  
 }
